92659e8330516094a93d7e21e2d8d29717e015f5
student-a502 <student-a502@STUD-RM5-A502>
2025-12-03 16:16:05 +0500

    РазрАбоТанный кЛасС отпрАВКи соОбщенИя на Почту. сАМков

---
+++ Classes/SendMail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REGIN.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("Lunch418@yandex.ru", "vvgcgwgofmbgwnhi"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("Lunch418@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
