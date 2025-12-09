87baca8e839d5b724d7f74933ee821afe0ed0fee
DadaKobeleva <kobelevadara@yandex.ru>
2025-12-09 13:09:46 +0500

    шаг 38-39 пикулев

--- Pages/Recovery.xaml.cs
+++ Pages/Recovery.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using REGIN.Classes;
 
 namespace REGIN.Pages
 {
@@ -20,9 +21,51 @@ namespace REGIN.Pages
     /// </summary>
     public partial class Recovery : Page
     {
+        string OldLogin;
+        bool IsCapture = false;
         public Recovery()
         {
             InitializeComponent();
+        }
+        private void CorrectLogin()
+        {
+            if (OldLogin != TbLogin.Text)
+            {
+                SetNotification("Hi, " + MainWindow.mainWindow.UserLogin.Name, Brushes.Black);
+
+                try
+                {
+                    BitmapImage bilImg = new BitmapImage();
+                    MemoryStream ms = new MemoryStream(MainWindow.mainWindow.UserLogin.Image);
+                    bilImg.BeginInit();
+                    bilImg.StreamSource = ms;
+                    bilImg.EndInit();
+                    ImageSource imgSrc = bilImg;
+
+                    DoubleAnimation StartAnimation = new DoubleAnimation();
+                    StartAnimation.From = 1;
+                    StartAnimation.To = 0;
+                    StartAnimation.Duration = TimeSpan.FromSeconds(0.6);
+                    StartAnimation.Completed += delegate
+                    {
+                        IUser.Source = imgSrc;
+
+                        DoubleAnimation EndAnimation = new DoubleAnimation();
+                        EndAnimation.From = 0;
+                        EndAnimation.To = 1;
+                        EndAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                        IUser.BeginAnimation(Image.OpacityProperty, EndAnimation);
+                    };
+                    IUser.BeginAnimation(Image.OpacityProperty, StartAnimation);
+                }
+                catch (Exception exp)
+                {
+                    Debug.WriteLine(exp.Message);
+                }
+
+                OldLogin = TbLogin.Text;
+                SendNewPassword();
+            }
         }
     }
 }
